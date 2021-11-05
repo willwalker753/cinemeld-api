b1ff09ba5391c48a76802045874688185f64af11
Will Walker <williamwalker753@gmail.com>
2021-11-05 01:39:49 -0500

    fixed account sign up error

--- src/app.js
+++ src/app.js
@@ -100,77 +100,83 @@ app.get('/similar', async (req, res) => {
 });
 
 app.post('/user/signup', async (req, res) => {
-    let responseArr = [];
-    let username = req.body.username;
-    let password = req.body.password;
-    let email = req.body.email;
-    if(username.length === 0) {
-        responseArr.push('username_empty');
-    } if(password.length === 0) {
-        responseArr.push('password_empty');
-    } else if(password.length < 6) {
-        responseArr.push('password_short');
-    } if(email.length === 0) {
-        responseArr.push('email_empty');
+    try {
+        let responseArr = [];
+        let username = req.body.username;
+        let password = req.body.password;
+        let email = req.body.email;
+        if(username.length === 0) {
+            responseArr.push('username_empty');
+        } if(password.length === 0) {
+            responseArr.push('password_empty');
+        } else if(password.length < 6) {
+            responseArr.push('password_short');
+        } if(email.length === 0) {
+            responseArr.push('email_empty');
+        }
+        sql = 'SELECT * FROM users WHERE username = ($1)';
+        params = [ username ];
+        pgResponse = await pool.query(sql, params);
+        if(pgResponse.rows.length !== 0) {
+            responseArr.push('username_taken')
+        }
+        sql = 'SELECT * FROM users WHERE email = ($1)';
+        params = [ email ];
+        pgResponse = await pool.query(sql, params);
+        if(pgResponse.rows.length !== 0) {
+            responseArr.push('email_taken')
+        }
+        if(responseArr.length === 0) {
+            let uid = short.generate();
+            let options = {
+                timeZone: 'America/Chicago',
+                year: 'numeric',
+                month: 'numeric',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+            }, formatter = new Intl.DateTimeFormat([], options);
+            let ts = formatter.format(new Date());
+            bcrypt.hash(password, 10, async function (err, hash) {   
+                sql = 'INSERT INTO users(username, password, email, ts, verified, uid) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+                params = [ username, hash, email, ts, false, uid ];
+                pgResponse = await pool.query(sql, params);
+                sql = 'CREATE TABLE '+uid+'(serial_id serial PRIMARY KEY, id VARCHAR ( 40 ) NOT NULL, poster_path VARCHAR( 100 ), type VARCHAR ( 20 ) NOT NULL, title VARCHAR ( 255 ) NOT NULL, tagline VARCHAR ( 100 ), runtime VARCHAR (50) )';
+                await pool.query(sql);
+                // let transporter = nodemailer.createTransport({
+                //     service: 'gmail',
+                //     auth: {
+                //         user: process.env.EM_EMAIL,
+                //         pass: process.env.EM_PASSWORD
+                //     }
+                // })
+                // let mailOptions = {
+                //     from: process.env.EMAIL,
+                //     to: email,
+                //     subject: 'Verify your email',
+                //     text: "Almost done, "+username+" To complete your Cinemeld sign up, we just need to verify your email address: "+email+". Once verified, you can start using all of Cinemeld's features and recover your password. Paste the following link into your browser: https://cinemeld.vercel.app/email/verify/cmiowqf8j901n3190 You’re receiving this email because you recently created a new Cinemeld account. If this wasn’t you, please ignore this email.",
+                //     html: '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="Content-Type" content="text/html charset=UTF-8" /> <link rel="preconnect" href="https://fonts.gstatic.com"> <link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto&display=swap" rel="stylesheet"> <style> body { background-color: #454545; font-family: "Lato", sans-serif; text-align: center; margin: 0; } #header-row { text-align:center; color: #E50914; background-color: #222222; width: 100%; margin-top: 0; } #logo { width: 35px; display: inline-block; } #title { display: inline-block; } #body { margin-top: 30px; margin-left:auto; margin-right:auto; color: #ffffff; background-color: #222222; width: 650px; padding-bottom: 30px; padding-top: 18px; } #body > a {text-align:center; align: center;} #body > a > button { background-color: #222222; margin-left: 210px; align: center; border: 2px solid #E50914; color: white; border-radius: 6px; padding: 4px 10px; font-size: 24px; margin-bottom: 15px; } #body > a > button:hover { cursor: pointer; background-color: #E50914; color: white; } #body > p { padding-left: 40px; padding-right: 40px; } #body-main { font-size: 18px; } .subtext { font-size: 14px; margin: 7px 0px 11px 0px } </style> </head> <body> <div id="header-row"> <img id="logo" src="https://github.com/willwalker753/cinemeld/blob/main/public/android-chrome-512x512.png?raw=true" alt="cinemeld logo"> <h1 id="title"> Cinemeld </h1> </div> <div id="body"> <p id="body-main">Almost done, <strong>'+username+'</strong> To complete your Cinemeld sign up, we just need to verify your email address: <strong>'+email+'</strong>.</p> <a href="https://cinemeld.vercel.app/email/verify/cmiowqf8j901n3190" target="_blank"> <button>Verify email address</button> </a> <p class="subtext">Once verified, you can start using all of Cinemeld&#39;s features and recover your password.</p> <p class="subtext">Button not working? Paste the following link into your browser: https://cinemeld.vercel.app/email/verify/cmiowqf8j901n3190</p> <p class="subtext">You&#39;re receiving this email because you recently created a new Cinemeld account. If this wasn&#39;t you, please ignore this email.</p> </div> </body> </html>'
+                // }
+                // transporter.sendMail(mailOptions, function(err, data){
+                //     if(err) {
+                //         console.log(err)
+                //     } else {
+                //         console.log('Verification email sent!')
+                //     }
+                // })
+                res.status(201).send({
+                    type: 'success', 
+                    data: pgResponse.rows
+                })
+            });
+        } else {
+            res.status(200).send({type:'fail', data:responseArr});
+        }
     }
-    sql = 'SELECT * FROM users WHERE username = ($1)';
-    params = [ username ];
-    pgResponse = await pool.query(sql, params);
-    if(pgResponse.rows.length !== 0) {
-        responseArr.push('username_taken')
-    }
-    sql = 'SELECT * FROM users WHERE email = ($1)';
-    params = [ email ];
-    pgResponse = await pool.query(sql, params);
-    if(pgResponse.rows.length !== 0) {
-        responseArr.push('email_taken')
-    }
-    if(responseArr.length === 0) {
-        let uid = short.generate();
-        let options = {
-            timeZone: 'America/Chicago',
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-        }, formatter = new Intl.DateTimeFormat([], options);
-        let ts = formatter.format(new Date());
-        bcrypt.hash(password, 10, async function (err, hash) {   
-            sql = 'INSERT INTO users(username, password, email, ts, verified, uid) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-            params = [ username, hash, email, ts, false, uid ];
-            pgResponse = await pool.query(sql, params);
-            sql = 'CREATE TABLE '+uid+'(serial_id serial PRIMARY KEY, id VARCHAR ( 40 ) NOT NULL, poster_path VARCHAR( 100 ), type VARCHAR ( 20 ) NOT NULL, title VARCHAR ( 255 ) NOT NULL, tagline VARCHAR ( 100 ), runtime VARCHAR (50) )';
-            await pool.query(sql);
-            let transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: process.env.EM_EMAIL,
-                    pass: process.env.EM_PASSWORD
-                }
-            })
-            let mailOptions = {
-                from: process.env.EMAIL,
-                to: email,
-                subject: 'Verify your email',
-                text: "Almost done, "+username+" To complete your Cinemeld sign up, we just need to verify your email address: "+email+". Once verified, you can start using all of Cinemeld's features and recover your password. Paste the following link into your browser: https://cinemeld.vercel.app/email/verify/cmiowqf8j901n3190 You’re receiving this email because you recently created a new Cinemeld account. If this wasn’t you, please ignore this email.",
-                html: '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="Content-Type" content="text/html charset=UTF-8" /> <link rel="preconnect" href="https://fonts.gstatic.com"> <link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto&display=swap" rel="stylesheet"> <style> body { background-color: #454545; font-family: "Lato", sans-serif; text-align: center; margin: 0; } #header-row { text-align:center; color: #E50914; background-color: #222222; width: 100%; margin-top: 0; } #logo { width: 35px; display: inline-block; } #title { display: inline-block; } #body { margin-top: 30px; margin-left:auto; margin-right:auto; color: #ffffff; background-color: #222222; width: 650px; padding-bottom: 30px; padding-top: 18px; } #body > a {text-align:center; align: center;} #body > a > button { background-color: #222222; margin-left: 210px; align: center; border: 2px solid #E50914; color: white; border-radius: 6px; padding: 4px 10px; font-size: 24px; margin-bottom: 15px; } #body > a > button:hover { cursor: pointer; background-color: #E50914; color: white; } #body > p { padding-left: 40px; padding-right: 40px; } #body-main { font-size: 18px; } .subtext { font-size: 14px; margin: 7px 0px 11px 0px } </style> </head> <body> <div id="header-row"> <img id="logo" src="https://github.com/willwalker753/cinemeld/blob/main/public/android-chrome-512x512.png?raw=true" alt="cinemeld logo"> <h1 id="title"> Cinemeld </h1> </div> <div id="body"> <p id="body-main">Almost done, <strong>'+username+'</strong> To complete your Cinemeld sign up, we just need to verify your email address: <strong>'+email+'</strong>.</p> <a href="https://cinemeld.vercel.app/email/verify/cmiowqf8j901n3190" target="_blank"> <button>Verify email address</button> </a> <p class="subtext">Once verified, you can start using all of Cinemeld&#39;s features and recover your password.</p> <p class="subtext">Button not working? Paste the following link into your browser: https://cinemeld.vercel.app/email/verify/cmiowqf8j901n3190</p> <p class="subtext">You&#39;re receiving this email because you recently created a new Cinemeld account. If this wasn&#39;t you, please ignore this email.</p> </div> </body> </html>'
-            }
-            transporter.sendMail(mailOptions, function(err, data){
-                if(err) {
-                    console.log(err)
-                } else {
-                    console.log('Verification email sent!')
-                }
-            })
-            res.status(201).send({
-                type: 'success', 
-                data: pgResponse.rows
-            })
-        });
-    } else {
-        res.status(200).send({type:'fail', data:responseArr});
+    catch(error) {
+        console.error(error)
+        res.status(500).send()
     }
 });
 
@@ -247,16 +253,6 @@ app.post('/user/favorites', async (req, res) => {
     params = [req.body.id];
     pgResponse = await pool.query(sql, params);
     res.status(200).send(pgResponse);
-});
-
-// temp endpoint remove
-app.get('/testingtesting', async (req, res) => {
-    console.log(req)
-    res.status(200).send();
-});
-app.post('/testingtesting', async (req, res) => {
-    console.log(req)
-    res.status(200).send();
 });
 
 app.use(function errorHandler(error, req, res, next) {
